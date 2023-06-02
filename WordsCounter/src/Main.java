96a575e1762dcf9c152e7c5ee2e86888c0464c6b
Mila Halko <milagalko1@gmail.com>
2023-06-02 14:44:09 +0300

    4.1 WordsLengthCounter code

--- WordsCounter/src/Main.java
+++ WordsCounter/src/Main.java
@@ -1,37 +1,29 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("Texts");
-        Folder folder = Folder.fromDirectory(file);
-        TextAnalizer textAnalizer = new TextAnalizer();
-
-        ArrayList<Integer> wordsLength = textAnalizer.getWordsLengthParallel(folder);
-//        long startTime = System.currentTimeMillis();
-//        long time = System.currentTimeMillis() - startTime;
-
-        double totalLength = 0;
-        double count = 0;
-        for (var item: wordsLength.entrySet()){
-            totalLength += (item.getValue() * item.getKey());
-            count += item.getValue();
+        File theGreatGatsbyPath = new File("Texts");
+        int[] threadsCount = {2, 4, 6, 8, 9};
+        for (var count : threadsCount) {
+            System.out.println("----------------------------------------------------------");
+            System.out.println(theGreatGatsbyPath.getAbsolutePath());
+            System.out.println("Threads count: " + count);
+            testResult(count, theGreatGatsbyPath);
         }
-        double meanLength = totalLength / count;
+    }
 
-        totalLength = 0;
-        for (var item: wordsLength.entrySet()){
-            totalLength += (Math.pow(item.getKey(),2) * item.getValue());
-        }
-        double D = (totalLength/count) - Math.pow(meanLength,2);
-        double G = Math.sqrt(D);
+    private static void testResult(int threadsCount, File path) throws IOException {
+        TextFileAnalyser theGreatGatsbyAnalyser = new TextFileAnalyser(threadsCount, path);
 
-        System.out.println("Count: " + count);
-        System.out.println("Mean length: " + meanLength);
-        System.out.println("Dispersion: " + D);
-        System.out.println("Mean square deviation: " + G);
-        System.out.println("\nTime: " + time);
+        long startTime = System.currentTimeMillis();
+        theGreatGatsbyAnalyser.getWordsLengthForkJoin();
+        long totalTime = System.currentTimeMillis() - startTime;
 
+        System.out.println("Count: " + theGreatGatsbyAnalyser.getWordsCount());
+        System.out.println("Mean length: " + theGreatGatsbyAnalyser.getMeanLength());
+        System.out.println("Min length: " + theGreatGatsbyAnalyser.getMinLength());
+        System.out.println("Max length: " + theGreatGatsbyAnalyser.getMaxLength());
+        System.out.println("\nTime: " + totalTime + " ms");
     }
 }
---
+++ WordsCounter/src/SearchTask.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.RecursiveTask;
+
+class SearchTask extends RecursiveTask<List<Integer>> {
+    private final File file;
+
+    SearchTask(File file) {
+        this.file = file;
+    }
+
+    @Override
+    protected List<Integer> compute() {
+        if (!file.isDirectory()) {
+            return getAllLengthsInFile(file);
+        }
+
+        ArrayList<Integer> wordsLength = new ArrayList<>();
+        List<RecursiveTask<List<Integer>>> tasks = new LinkedList<>();
+
+        for (File entry : Objects.requireNonNull(file.listFiles())) {
+            SearchTask task = new SearchTask(entry);
+            tasks.add(task);
+            task.fork();
+        }
+
+        for (RecursiveTask<List<Integer>> task : tasks) {
+            wordsLength.addAll(task.join());
+        }
+
+        return wordsLength;
+    }
+
+    private static List<Integer> getAllLengthsInFile(File file) {
+        List<Integer> wordLengths = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line = reader.readLine();
+            while (line != null) {
+                for (String word : getAllWordsInLine(line)) {
+                    wordLengths.add(word.length());
+                }
+                line = reader.readLine();
+            }
+        } catch (Exception ignored) {
+        }
+
+        return wordLengths;
+    }
+
+    private static String[] getAllWordsInLine(String line) {
+        return line.trim().split("(\\s|\\p{Punct})+");
+    }
+}
